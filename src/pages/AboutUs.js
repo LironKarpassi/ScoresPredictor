96a4532f4db2a176df64b21dca48d7d934190a99
Liron <liron.karpassi@gmail.com>
2021-12-06 20:21:52 +0200

    withphoto

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -5,27 +5,19 @@ const AboutUs = () => {
         <div className="AboutUsbackground">
             <div className= "TeamMembers">
                 <div className="Liron">
-                    <div className="Photo">
-                        <h2>PHOTO</h2>
-                    </div>
+                    <div className="Photo"></div>
                     <div className="AboutMember"><h2>Karpassi Liron</h2></div>
                 </div>
                 <div className="Ido">
-                <div className="Photo">
-                        <h2>PHOTO</h2>
-                    </div>
+                <div className="Photo"></div>
                     <div className="AboutMember"><h2>Itzhaki Ido</h2></div>
                 </div>
                 <div className="Michal">
-                <div className="Photo">
-                        <h2>PHOTO</h2>
-                    </div>
+                <div className="Photo"></div>
                     <div className="AboutMember"><h2>Verchivkin Michal</h2></div>
                 </div>
                 <div className="Ran">
-                <div className="Photo">
-                        <h2>PHOTO</h2>
-                    </div>
+                <div className="Photo"></div>
                     <div className="AboutMember"><h2>Moshe Ran</h2></div>
                 </div>
             </div>
