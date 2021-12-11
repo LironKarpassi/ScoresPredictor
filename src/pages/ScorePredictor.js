05d67485478485fee591a9b1b2d97d0635876962
Liron <liron.karpassi@gmail.com>
2021-12-11 13:26:05 +0200

    SelectedItemsInDropdown

--- src/pages/ScorePredictor.js
+++ src/pages/ScorePredictor.js
@@ -1,75 +1,80 @@
 import React,  {useState } from "react";
 import { Dropdown, Option } from "./Dropdown.js";
-
+import './Scorepredictor.css';
 const ScorePredictor = () => {
     const [optionValue, setOptionValue] = useState("");
     const handleSelect = (e) => {
     console.log(e.target.value);
     setOptionValue(e.target.value);
+    
   };
     return (
-        <div>
-          <h1>Which Groups Are You Insterested To Predict?</h1>
+        <div className= "Container">
+          <h1>Which Teams Are You Insterested To Predict?</h1>
+          <div className="groups">
           <Dropdown
-            formLabel="Choose a group"
-            buttonText="Send form"
+            formLabel="Choose Home Team"
+            buttonText="Submit Home Team"
             onChange={handleSelect}
             action="https://jsonplaceholder.typicode.com/posts"
           >
             <Option selected value="Click to see options" />
-            <Option value="Option 1" />
-            <Option value="Option 2" />
-            <Option value="Option 3" />
-            <Option value="Option 4" />
-            <Option value="Option 5" />
-            <Option value="Option 6" />
-            <Option value="Option 7" />
-            <Option value="Option 8" />
-            <Option value="Option 9" />
-            <Option value="Option 10" />
-            <Option value="Option 11" />
-            <Option value="Option 12" />
-            <Option value="Option 13" />
-            <Option value="Option 14" />
-            <Option value="Option 15" />
-            <Option value="Option 16" />
-            <Option value="Option 17" />
-            <Option value="Option 18" />
-            <Option value="Option 19" />
-            <Option value="Option 20" />
+            <Option value="Sunderland" />
+            <Option value="Chelsea" />
+            <Option value="Wolverhampton Wanderers" />
+            <Option value="Everton" />
+            <Option value="Birmingham City" />
+            <Option value="West Bromwich Albion" />
+            <Option value="Stoke City" />
+            <Option value="Fulham" />
+            <Option value="Manchester City" />
+            <Option value="Arsenal" />
+            <Option value="Newcastle United" />
+            <Option value="West Ham United" />
+            <Option value="Manchester United" />
+            <Option value="Aston Villa" />
+            <Option value="Tottenham Hotspur" />
+            <Option value="Liverpool" />
+            <Option value="Swansea City" />
+            <Option value="Norwich City" />
+            <Option value="Queens Park Rangers" />
+            <Option value="Southampton" />
 
           </Dropdown>
           <p>You selected {optionValue} </p>
+          </div>
+          <div className="groups">
           <Dropdown
-            formLabel="VS Choose a group"
-            buttonText="Send form"
+            formLabel="Choose Away Team"
+            buttonText="Submit Away Team"
             onChange={handleSelect}
             action="https://jsonplaceholder.typicode.com/posts"
           >
             <Option selected value="Click to see options" />
-            <Option value="Option 1" />
-            <Option value="Option 2" />
-            <Option value="Option 3" />
-            <Option value="Option 4" />
-            <Option value="Option 5" />
-            <Option value="Option 6" />
-            <Option value="Option 7" />
-            <Option value="Option 8" />
-            <Option value="Option 9" />
-            <Option value="Option 10" />
-            <Option value="Option 11" />
-            <Option value="Option 12" />
-            <Option value="Option 13" />
-            <Option value="Option 14" />
-            <Option value="Option 15" />
-            <Option value="Option 16" />
-            <Option value="Option 17" />
-            <Option value="Option 18" />
-            <Option value="Option 19" />
-            <Option value="Option 20" />
+            <Option value="Sunderland" />
+            <Option value="Chelsea" />
+            <Option value="Wolverhampton Wanderers" />
+            <Option value="Everton" />
+            <Option value="Birmingham City" />
+            <Option value="West Bromwich Albion" />
+            <Option value="Stoke City" />
+            <Option value="Fulham" />
+            <Option value="Manchester City" />
+            <Option value="Arsenal" />
+            <Option value="Newcastle United" />
+            <Option value="West Ham United" />
+            <Option value="Manchester United" />
+            <Option value="Aston Villa" />
+            <Option value="Tottenham Hotspur" />
+            <Option value="Liverpool" />
+            <Option value="Swansea City" />
+            <Option value="Norwich City" />
+            <Option value="Queens Park Rangers" />
+            <Option value="Southampton" />
 
           </Dropdown>
           <p>You selected {optionValue} </p>
+        </div>
         </div>
       );  
    
